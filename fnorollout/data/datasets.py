8af90d49da42f65d2911bf0445f834b1a3ce7842
paritoshborkar <paritoshpborkar@gmail.com>
2026-08-10 05:05:56 +0530

    Load fluid trajectory dataset to torch

--- fnorollout/data/datasets.py
+++ fnorollout/data/datasets.py
@@ -1,1 +1,39 @@
 """Dataset classes for loading trajectory data (e.g. GeophysicalFlows.jl NetCDF output)."""
+
+from pathlib import Path
+
+import torch
+import xarray as xr
+from torch.utils.data import Dataset
+
+
+class TrajectoryDataset(Dataset):
+    """
+    Dataset to track a fluid flow variable like vorticity over time.
+    Follows the dataset format required by the neuralop package trainer
+    """
+
+    def __init__(self, path: Path, rollout_steps: int = 1, channel_dim=1):
+        super().__init__()
+        dataset_nc = xr.open_dataset(path)  # y, x, time index order
+
+        zeta = torch.from_numpy(dataset_nc["zeta"].values).float()  # Make float32 dtype
+        zeta = zeta.permute(2, 1, 0)  # y,x,time -> time,x,y
+        self.zeta = zeta.unsqueeze(channel_dim)  # time,x,y -> time, channel=1, x, y
+        self.rollout_steps = rollout_steps
+
+    def __len__(self):
+        return self.zeta.shape[0] - self.rollout_steps
+
+    def __getitem__(self, index: int):
+        """
+        Returns a snapshot with key value "x"
+        and the next snapshot in the rollout step with key value "y"
+
+        x and y are used to match with expected keys by neuralop package,
+        not to be confused with x and y axes
+        """
+        return {
+            "x": self.zeta[index], 
+            "y": self.zeta[index + self.rollout_steps]
+            }
--- fnorollout/scripts/train.py
+++ fnorollout/scripts/train.py
@@ -13,9 +13,37 @@ import hydra
 import torch
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
+from torch.utils.data import DataLoader, Subset
+
+from fnorollout.scripts.data import load_dataset_from_file
 
 
-@hydra.main(version_base=None, config_path="../configs", config_name="config")
+def create_dataloaders(data_config: DictConfig):
+    dataset = load_dataset_from_file(data_config=data_config)
+
+    train_split = data_config.split.train
+    val_split = data_config.split.val
+
+    train_index = int(len(dataset) * train_split)
+    val_index = train_index + int(len(dataset) * val_split)
+
+    train_loader = DataLoader(
+        dataset=Subset(dataset, range(train_index)), batch_size=data_config.dataloader.train.batch_size
+    )
+    val_loader = DataLoader(
+        dataset=Subset(dataset, range(train_index, val_index)), batch_size=data_config.dataloader.val.batch_size
+    )
+
+    test_loader = DataLoader(
+        dataset=Subset(dataset, range(val_index, len(dataset))), batch_size=data_config.dataloader.test.batch_size
+    )
+
+    test_resolution = dataset[0]["x"].shape[-1]
+    test_loaders = {test_resolution: test_loader}
+    return train_loader, val_loader, test_loaders
+
+
+@hydra.main(version_base=None, config_path="../../configs", config_name="config")
 def main(config: DictConfig) -> None:
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {DEVICE}")
@@ -28,16 +56,12 @@ def main(config: DictConfig) -> None:
     model_config = config.model
     train_config = config.training
 
+    train_dataloader, val_dataloader, test_dataloaders = create_dataloaders(
+        data_config=data_config
+    )
+
 
 if __name__ == "__main__":
-    # Load data
-    ## Using data config values, load the correct data file
-    ## Convert from NETcdf to pytorch tensors
-
-    # Preprocessing
-    ## Normalize data, convert data from column to row major
-    ## Add layer of positional coordinates
-
     # Instantiate model and optimizer
 
     # Create training loop
